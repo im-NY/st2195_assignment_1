1bcbab5cbdfeb88c9c02e37da0f9a2a1109a7b3d
Yap Ning Yi <nyyap002@mymail.sim.edu.sg>
2022-10-09 14:38:45 +0800

    Updated main with files from master branch

---
+++ spyder/script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 15:25:18 2022
+
+@author: yapni
+"""
+
+#to print integers from 1 to 10 (including 1 and 10) using for loop
+
+for i in range(1,11):
+    print(i)
+    
